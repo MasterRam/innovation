ee1c5db9d8767354805f86ca6495a5099c100eb3
masterram <ramkumar.murugarasan@hotmail.com>
2019-01-19 11:36:52 -0800

    updated temp folder for unit test

--- SERVER/node-mongo-apis/build/clean.js
+++ SERVER/node-mongo-apis/build/clean.js
@@ -1,6 +1,6 @@
 const del = require('del');
 
-del(['lib/**/*.js', 'lib/**/*.js.map', 'test/**/*.spec.js', 'test/**/*.spec.js.map']).then(paths => {
+del(['dist.test']).then(paths => {
     console.log('----------------------------------------');
     console.log("Deleted " + paths.length + " temp files (.js,.js.map).");
     console.log('----------------------------------------');
