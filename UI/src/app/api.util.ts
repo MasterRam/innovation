591d2a7ba9dccc13961a96e568fcdcf3c53522a9
Ramkumar Murugarasan <Ramkumar.Murugarasan@rencata.com>
2019-01-28 09:41:59 -0800

    added service

--- UI/src/app/api.util.ts
+++ UI/src/app/api.util.ts
@@ -1,83 +1,83 @@
 const apiConst = [
-    {
-        name: 'product-api',
-        domain: 'localhost',
-        port: '3001',
-        suffix: undefined,
-        isHTTPS: false,
-        headers: {
-            ['content-type']: 'application/x-www-form-urlencoded'
-        }
-    },
-    {
-        name: 'identity',
-        domain: 'localhost',
-        port: '5000',
-        // suffix: '',
-        isHTTPS: false,
-        headers: {
-            ['content-type']: 'application/x-www-form-urlencoded'
-        }
-    },
-    {
-        name: 'blog-json',
-        domain: 'localhost',
-        port: '4200',
-        suffix: 'assets/samples/blog',
-        isHTTPS: false,
-        headers: {
-            ['content-type']: 'application/x-www-form-urlencoded'
-        }
+  {
+    name: "product-api",
+    domain: "localhost",
+    port: "3001",
+    suffix: undefined,
+    isHTTPS: false,
+    headers: {
+      ["content-type"]: "application/x-www-form-urlencoded"
     }
+  },
+  {
+    name: "identity",
+    domain: "localhost",
+    port: "5000",
+    // suffix: '',
+    isHTTPS: false,
+    headers: {
+      ["content-type"]: "application/x-www-form-urlencoded"
+    }
+  },
+  {
+    name: "blog-json",
+    domain: "localhost",
+    port: "4200",
+    suffix: "assets/samples/blog",
+    isHTTPS: false,
+    headers: {
+      ["content-type"]: "application/x-www-form-urlencoded"
+    }
+  }
 ];
 
 export interface IApiParam {
-    urlObj: IDataLoadURL;
-    uiElement?: string;
-    body?: any;
-    queryParam?: object;
+  urlObj: IDataLoadURL;
+  uiElement?: string;
+  body?: any;
+  queryParam?: object;
 }
 export interface IDataLoadURL {
-    method: string;
-    url: string;
-    hostName: string;
-    uiElement: string;
-    defaultHeaders: any;
+  method: "POST" | "GET" | "DELETE" | "PUT";
+  url: string;
+  hostName: string;
+  uiElement: string;
+  defaultHeaders: any;
 }
 
 export const GetFullURL = (hostName: string, url: string): string => {
-    const apiData = apiConst.find(t => t.name === hostName);
-    if (apiData === undefined) {
-        throw Error('Host data not defined');
-    }
+  const apiData = apiConst.find(t => t.name === hostName);
+  if (apiData === undefined) {
+    throw Error("Host data not defined");
+  }
 
-    const http = apiData.isHTTPS ? 'https://' : 'http://';
-    const port = apiData.port === undefined ? '' : `:${apiData.port}`;
-    const suffix = apiData.suffix === undefined ? '' : `/${apiData.suffix}`;
-    return `${http + apiData.domain + port}${suffix}/${url}`;
+  const http = apiData.isHTTPS ? "https://" : "http://";
+  const port = apiData.port === undefined ? "" : `:${apiData.port}`;
+  const suffix = apiData.suffix === undefined ? "" : `/${apiData.suffix}`;
+  return `${http + apiData.domain + port}${suffix}/${url}`;
 };
 
 export const GetHeaders = (hostName: string): object => {
-    const apiData = apiConst.find(t => t.name === hostName);
-    if (apiData === undefined) {
-        throw Error('Host data not defined');
-    }
-    if (apiData.headers === undefined) { return {}; }
-    const tempHeader = {};
-    Object.assign(tempHeader, apiData.headers);
-    return tempHeader;
+  const apiData = apiConst.find(t => t.name === hostName);
+  if (apiData === undefined) {
+    throw Error("Host data not defined");
+  }
+  if (apiData.headers === undefined) {
+    return {};
+  }
+  const tempHeader = {};
+  Object.assign(tempHeader, apiData.headers);
+  return tempHeader;
 };
 
-
 const reset = () => {
-    const gettoken = localStorage.getItem('il.a_tk');
-    if (gettoken) {
-        apiConst.forEach(t => {
-            t.headers['Authorization'] = gettoken;
-        });
-    } else {
-
-    }
+  const gettoken = localStorage.getItem("il.a_tk");
+  if (gettoken) {
+    apiConst.forEach(t => {
+      t.headers["Authorization"] = gettoken;
+    });
+  } else {
+  }
 };
 
 reset();
