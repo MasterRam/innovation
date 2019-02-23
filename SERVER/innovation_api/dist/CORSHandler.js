b4fddf2b05ed39972393f07b742376e06fa6ff85
masterram <ramkumar.murugarasan@hotmail.com>
2019-02-23 10:30:38 -0800

    removed dist folder from git

--- SERVER/innovation_api/dist/CORSHandler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CorsOptionsLocal = {
-    origin: 'http://localhost:4200',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-//# sourceMappingURL=CORSHandler.js.map
--- SERVER/innovation_api/dist/RND/function.rnd.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.testFunc = (prop, day) => { };
-//# sourceMappingURL=function.rnd.js.map
