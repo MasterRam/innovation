ba68f7a8837f07e60cc03a7a8f56d8c23c76f6e1
masterram <ramkumar.murugarasan@hotmail.com>
2019-02-23 11:41:17 -0800

    removed identity from source

--- SERVER/node-identity/lib/models/documents.ts
+++
@@ -1,4 +0,0 @@
-import { model } from 'mongoose';
-import { UserSchema } from '../modules/auth0/user.schema';
-
-export const UserDocument = model('users', UserSchema);
--- SERVER/node-identity/lib/repository/base.repository.ts
+++
@@ -1,9 +0,0 @@
-export interface IRepository<T> {
-  findBy(findQuery: any, callback: (success: boolean, response: any) => void);
-
-  Add(
-    findQuery: any,
-    data: T,
-    callback: (success: boolean, response: any) => void
-  );
-}
